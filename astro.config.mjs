20c8704a2b522ca6c69d0a086b0a2287cae17013
Angus <angus.quigley@icloud.com>
2025-07-17 20:35:24 +1200

    added a sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://www.angusq.com/',
+  integrations: [tailwind(), sitemap()],
   // Update the viewTransitions object to include the 'fallback'
   viewTransitions: {
     fallback: 'animate'
